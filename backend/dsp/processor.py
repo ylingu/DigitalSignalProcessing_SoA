5bef8216676d3a16a8fdf6e38d62be1480df7f58
Ylingu <wzorlite@gmail.com>
2024-04-17 15:01:25 +0800

    Refactor signal generator and waveform drawer for signal generation and visualization

--- backend/dsp/processor.py
+++ backend/dsp/processor.py
@@ -1,82 +1,9 @@
 import numpy as np
 from enum import Enum, auto
-from scipy import signal
+import scipy.signal as sgl
+from scipy.io import wavfile
 import matplotlib.pyplot as plt
-
-
-class SignalGenerator:
-    def __init__(
-        self,
-        amplitude: float,
-        frequency: float,
-        duration: tuple[float, float],
-        sampling_rate: float = 1000,
-        noise_amplitude: float = 0,
-    ) -> None:
-        self.amplitude = amplitude
-        self.frequency = frequency
-        self.duration = duration
-        self.sampling_rate = sampling_rate
-        self.noise_amplitude = noise_amplitude
-        self.t = np.linspace(
-            duration[0],
-            duration[1],
-            int(sampling_rate * (duration[1] - duration[0])),
-            endpoint=False,
-        )
-
-    def generate_noise(self) -> np.ndarray:
-        return np.random.normal(scale=self.noise_amplitude, size=self.t.shape)
-
-    def generate_sine_wave(self, b: float = 0) -> np.ndarray:
-        return (
-            self.amplitude * np.sin(2 * np.pi * self.frequency * self.t + b)
-            + self.generate_noise()
-        )
-
-    def generate_rectangular_pulse(self, pulse_width: float) -> np.ndarray:
-        return (
-            self.amplitude
-            * (np.sign(self.t + pulse_width / 2) - np.sign(self.t - pulse_width / 2))
-            / 2
-            + self.generate_noise()
-        )
-
-    def generate_triangular_wave(self, width: int = 1) -> np.ndarray:
-        return (
-            self.amplitude
-            * signal.sawtooth(2 * np.pi * self.frequency * self.t, width=width)
-            + self.generate_noise()
-        )
-
-    def generate_square_wave(self, duty: float = 0.5) -> np.ndarray:
-        return (
-            self.amplitude
-            * signal.square(2 * np.pi * self.frequency * self.t, duty=duty)
-            + self.generate_noise()
-        )
-
-    @staticmethod
-    def sample(
-        signal: np.ndarray, sampling_rate: int, duration: tuple[float, float]
-    ) -> np.ndarray:
-        t = np.linspace(
-            duration[0],
-            duration[1],
-            int(sampling_rate * (duration[1] - duration[0])),
-            endpoint=False,
-        )
-        return np.interp(
-            t, np.linspace(0, duration[1] - duration[0], len(signal)), signal
-        )
-
-    @staticmethod
-    def quantize(signal: np.ndarray, levels: int) -> np.ndarray:
-        min_val = np.min(signal)
-        max_val = np.max(signal)
-        q = np.linspace(min_val, max_val, levels)
-        quantized_signal = q[np.argmin(np.abs(q[:, None] - signal), axis=0)]
-        return quantized_signal
+import copy
 
 
 class SignalType(Enum):
@@ -84,36 +11,140 @@ class SignalType(Enum):
     RECTANGULAR_PULSE = auto()
     TRIANGULAR = auto()
     SQUARE = auto()
-    QUANTIZED = auto()
+    NOISE = auto()
+    OTHER = auto()
 
 
-class WaveformPlotter:
+class Signal:
     def __init__(
         self,
-        t_list: list[np.ndarray],
-        signals: list[np.ndarray],
-        signal_types: list[SignalType],
+        t: np.ndarray,
+        signal: np.ndarray,
+        duration: tuple[float, float],
+        type: SignalType = SignalType.SINE,
+        sampling_rate: int = 1000,
+        is_digitial: bool = False,
     ) -> None:
-        assert len(t_list) == len(signals) == len(signal_types)
-        self.t_list = t_list
+        self.t = t
+        self.signal = signal
+        self.duration = duration
+        self.type = type
+        self.sampling_rate = sampling_rate
+        self.is_digitial = is_digitial
+
+    def sample(self, sampling_rate: int) -> None:
+        t = self.t
+        self.t = SignalGenerator.get_t(self.duration, sampling_rate)
+        self.signal = np.interp(self.t, t, self.signal)
+
+    def quantize(self, levels: int) -> None:
+        min_val = np.min(self.signal)
+        max_val = np.max(self.signal)
+        q = np.linspace(min_val, max_val, levels)
+        self.signal = q[np.argmin(np.abs(q[:, None] - self.signal), axis=0)]
+        self.is_digitial = True
+
+
+class SignalGenerator:
+    @staticmethod
+    def get_t(duration: tuple[float, float], sampling_rate: int = 1000) -> np.ndarray:
+        return np.linspace(
+            duration[0],
+            duration[1],
+            int(sampling_rate * (duration[1] - duration[0])),
+            endpoint=False,
+        )
+
+    @staticmethod
+    def generate_noise(
+        amplitude: float, duration: tuple[float, float], sampling_rate: int = 1000
+    ) -> Signal:
+        t = SignalGenerator.get_t(duration, sampling_rate)
+        signal = np.random.normal(scale=amplitude, size=t.shape)
+        return Signal(t, signal, duration, SignalType.NOISE, sampling_rate)
+
+    @staticmethod
+    def generate_sine_wave(
+        amplitude: float,
+        frequency: float,
+        duration: tuple[float, float],
+        sampling_rate: int = 1000,
+        bias: float = 0,
+    ) -> Signal:
+        t = SignalGenerator.get_t(duration, sampling_rate)
+        signal = amplitude * np.sin(2 * np.pi * frequency * t + bias)
+        return Signal(t, signal, duration, SignalType.SINE, sampling_rate)
+
+    @staticmethod
+    def generate_rectangular_pulse(
+        amplitude: float,
+        duration: tuple[float, float],
+        pulse_width: float,
+        sampling_rate: int = 1000,
+    ) -> Signal:
+        t = SignalGenerator.get_t(duration, sampling_rate)
+        signal = (
+            amplitude
+            * (np.sign(t + pulse_width / 2) - np.sign(t - pulse_width / 2))
+            / 2
+        )
+        return Signal(t, signal, duration, SignalType.RECTANGULAR_PULSE, sampling_rate)
+
+    @staticmethod
+    def generate_triangular_wave(
+        amplitude: float,
+        frequency: float,
+        duration: tuple[float, float],
+        sampling_rate: int = 1000,
+        width: float = 1,
+    ) -> Signal:
+        t = SignalGenerator.get_t(duration, sampling_rate)
+        signal = amplitude * sgl.sawtooth(2 * np.pi * frequency * t, width=width)
+        return Signal(t, signal, duration, SignalType.TRIANGULAR, sampling_rate)
+
+    @staticmethod
+    def generate_square_wave(
+        amplitude: float,
+        frequency: float,
+        duration: tuple[float, float],
+        sampling_rate: int = 1000,
+        duty: float = 0.5,
+    ) -> Signal:
+        t = SignalGenerator.get_t(duration, sampling_rate)
+        signal = amplitude * sgl.square(2 * np.pi * frequency * t, duty=duty)
+        return Signal(t, signal, duration, SignalType.SQUARE, sampling_rate)
+
+
+class Plotter:
+    def __init__(
+        self,
+        signals: list[Signal],
+    ) -> None:
         self.signals = signals
-        self.signal_types = signal_types
-        self.fig, self.axs = plt.subplots(len(t_list), 1, figsize=(12, 3 * len(t_list)))
-        if len(t_list) == 1:
+        self.fig, self.axs = plt.subplots(
+            len(signals), 1, figsize=(10, 2.5 * len(signals))
+        )
+        if len(signals) == 1:
             self.axs = [self.axs]
 
     def plot_waveforms(self) -> None:
-        for ax, t, sig, type in zip(
-            self.axs, self.t_list, self.signals, self.signal_types
-        ):
-            if type == SignalType.QUANTIZED:
+        for ax, signal in zip(self.axs, self.signals):
+            if signal.is_digitial:
                 ax.step(
-                    t, sig, where="mid", label=f"{type.name.capitalize()}", color="blue"
+                    signal.t,
+                    signal.signal,
+                    where="mid",
+                    label=f"{signal.type.name.capitalize()}",
+                    color="blue",
                 )
             else:
-                ax.plot(t, sig, label=f"{type.name.capitalize()}", color="blue")
-                ax.scatter(t, sig, color="red", s=10)
-            ax.set_title(f"{type.name.capitalize()} Signal")
+                ax.plot(
+                    signal.t,
+                    signal.signal,
+                    label=f"{signal.type.name.capitalize()}",
+                    color="blue",
+                )
+                ax.scatter(signal.t, signal.signal, color="red", s=10)
             ax.set_xlabel("Time (s)")
             ax.set_ylabel("Amplitude")
             ax.legend()
@@ -121,73 +152,77 @@ class WaveformPlotter:
         plt.tight_layout()
         plt.show()
 
+    def plot_spectrums(self) -> None:
+        for ax, signal in zip(self.axs, self.signals):
+            freq, spectrum = sgl.welch(signal.signal, fs=signal.sampling_rate)
+            ax.semilogy(freq, spectrum)
+            ax.set_xlabel("Frequency [Hz]")
+            ax.set_ylabel("PSD [V**2/Hz]")
+        plt.tight_layout()
+        plt.show()
+
 
 def main():
-    sine_noise = SignalGenerator(
-        amplitude=1, frequency=5, duration=(0, 1), noise_amplitude=0.1
-    )
-    sine = SignalGenerator(amplitude=5, frequency=3, duration=(0, 1))
-    rect_pulse = SignalGenerator(amplitude=13, frequency=2, duration=(-6, 6))
-    triangular = SignalGenerator(amplitude=1, frequency=0.125, duration=(0, 12))
-    square = SignalGenerator(amplitude=1, frequency=1 / 6, duration=(0, 12))
+    sine = SignalGenerator.generate_sine_wave(5, 3, (0, 10))
+    sine_noise = copy.deepcopy(sine)
+    sine_noise.signal += SignalGenerator.generate_noise(1, (0, 10)).signal
+    rect_pulse = SignalGenerator.generate_rectangular_pulse(13, (-6, 6), 2)
+    triangular = SignalGenerator.generate_triangular_wave(1, 0.125, (0, 12), 1000, 0.7)
+    square = SignalGenerator.generate_square_wave(1, 1 / 6, (0, 12), 1000, 0.3)
 
-    WaveformPlotter(
-        [sine_noise.t, sine.t, rect_pulse.t, triangular.t, square.t],
-        [
-            sine_noise.generate_sine_wave(),
-            sine.generate_sine_wave(),
-            rect_pulse.generate_rectangular_pulse(2),
-            triangular.generate_triangular_wave(0.7),
-            square.generate_square_wave(0.3),
-        ],
-        [
-            SignalType.SINE,
-            SignalType.SINE,
-            SignalType.RECTANGULAR_PULSE,
-            SignalType.TRIANGULAR,
-            SignalType.SQUARE,
-        ],
-    ).plot_waveforms()
+    Plotter([sine, sine_noise, rect_pulse, triangular, square]).plot_waveforms()
 
-    sine_signal = SignalGenerator(
-        amplitude=1, frequency=0.1, duration=(0, 5)
-    ).generate_sine_wave() + SignalGenerator(
-        amplitude=4, frequency=0.125, duration=(0, 5)
-    ).generate_sine_wave(np.pi / 2)
-    quantized_signal = SignalGenerator.quantize(sine_signal, 8)
-    WaveformPlotter(
-        [
-            SignalGenerator(amplitude=1, frequency=0.1, duration=(0, 5)).t,
-            SignalGenerator(amplitude=1, frequency=0.1, duration=(0, 5)).t,
-        ],
-        [sine_signal, quantized_signal],
-        [SignalType.SINE, SignalType.QUANTIZED],
-    ).plot_waveforms()
+    sine = SignalGenerator.generate_sine_wave(1, 0.1, (0, 5))
+    sine.signal += SignalGenerator.generate_sine_wave(
+        4, 0.125, (0, 5), bias=np.pi / 2
+    ).signal
+    quantized_sine = copy.copy(sine)
+    quantized_sine.quantize(8)
+    Plotter([sine, quantized_sine]).plot_waveforms()
 
-    sine1 = SignalGenerator(amplitude=1, frequency=100, duration=(0, 0.5))
-    sine2 = SignalGenerator(amplitude=1, frequency=10, duration=(0, 0.5))
+    sine1 = SignalGenerator.generate_sine_wave(1, 100, (0, 0.5))
+    sine2 = SignalGenerator.generate_sine_wave(1, 10, (0, 0.5))
+    convolved_signal = np.convolve(sine1.signal, sine2.signal)
     # 卷积
-    sine3 = np.convolve(sine1.generate_sine_wave(), sine2.generate_sine_wave())
-    WaveformPlotter(
-        [sine1.t, sine2.t, np.linspace(-0.5, 1, len(sine3))],
-        [sine1.generate_sine_wave(), sine2.generate_sine_wave(), sine3],
-        [SignalType.SINE, SignalType.SINE, SignalType.SINE],
-    ).plot_waveforms()
-
-    sine = SignalGenerator(
-        amplitude=1, frequency=10, duration=(0, 1), sampling_rate=100
+    sine3 = Signal(
+        np.linspace(0, 1, len(convolved_signal)),
+        convolved_signal,
+        (0, 1),
+        SignalType.OTHER,
     )
-    sine_signal = sine.generate_sine_wave(0.3)
-    t_list, signals = [sine.t], [sine_signal]
+    Plotter([sine1, sine2, sine3]).plot_waveforms()
+
+    sine = SignalGenerator.generate_sine_wave(1, 10, (0, 1), 100)
+    signals = [sine]
     for sampling_rate in [5, 10, 20, 40, 80, 100]:
-        t_list.append(
-            SignalGenerator(
-                amplitude=1, frequency=10, duration=(0, 1), sampling_rate=sampling_rate
-            ).t
-        )
-        signals.append(SignalGenerator.sample(sine_signal, sampling_rate, (0, 1)))
-        print(f"Signal sampled at {sampling_rate} Hz")
-    WaveformPlotter(t_list, signals, [SignalType.SINE] * len(signals)).plot_waveforms()
+        temp = copy.copy(sine)
+        temp.sample(sampling_rate)
+        signals.append(copy.copy(temp))
+    Plotter(signals).plot_waveforms()
+
+    sampling_rate, signal = wavfile.read("origin.wav")
+    signal = signal[:, 0]
+    wav = Signal(
+        np.linspace(0, len(signal) / sampling_rate, len(signal)),
+        signal,
+        (0, len(signal) / sampling_rate),
+        SignalType.OTHER,
+        sampling_rate,
+    )
+    signals = [wav]
+    for sampling_rate in [441, 882, 2205, 11025, 22050]:
+        temp = copy.copy(wav)
+        temp.sample(sampling_rate)
+        signals.append(copy.copy(temp))
+    Plotter(signals).plot_waveforms()
+    Plotter(signals).plot_spectrums()
+
+    downsampled = copy.copy(wav)
+    downsampled.sample(8820)
+    resampled = copy.copy(downsampled)
+    resampled.sample(44100)
+    Plotter([wav, downsampled, resampled]).plot_waveforms()
+    Plotter([wav, downsampled, resampled]).plot_spectrums()
 
 
 if __name__ == "__main__":
